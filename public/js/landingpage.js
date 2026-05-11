708a6624156ece6f05a19f795e601ca3abc54956
hadassakelly <hadassak355@gmail.com>
2026-05-11 20:05:15 -0300

    feat/fix: inicio outro teste carrossel, atualiza a animação do fio, criar a div Stitchify

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -1,74 +1,4 @@
-// Passando elementos HTML para variavies JavaScript
-const slider = document.querySelector('.cards-blog');
-const sliderContent = document.querySelector('.slider-conteudo');
-const leftArrow = document.getElementById("seta-esquerda");
-const rightArrow = document.getElementById("seta-direita");
 
-// Declarar variáveis globais
-let currentPage = 0;
-let itensPerView = 1;
-let autoSlideInterval;
-
-// Criação/Organização do Carrossel
-
-function updateCarrossel(){
-    const sliderWidth = slider.offsetWidth;
-    const itemWidth = sliderContent.children[0].getBoundingClientRect().width;
-
-    itensPerView = (sliderWidth / itemWidth);
-    totalPages = Math.ceil((sliderContent.children.length / itensPerView) - 2* ((sliderWidth/itemWidth)/100));
-    
-}
-
-// MOVIMENTAÇÃO
-
-function scrollToPage(){
-    const cardWidth = sliderContent.children[0].getBoundingClientRect().width + 20;
-    const newPosition = cardWidth * 3 * currentPage;
-    sliderContent.scrollTo({left:newPosition, behavior: "smooth"});
-}
-
-function moveLeft(){
-    currentPage--;
-    if (currentPage < 0){
-        currentPage = totalPages-1;
-    }
-    scrollToPage();
-    resetAutoSlide();
-}
-
-function moveRight(){
-    currentPage++;
-    if (currentPage >= totalPages){
-        currentPage = 0;
-    }
-
-    scrollToPage();
-    resetAutoSlide();
-}
-
-//SLIDE AUTOMATICO
-
-function startAutoSlide(){
-    autoSlideInterval = setInterval( () => {
-        moveRight();
-    }, 10000)
-}
-
-function resetAutoSlide(){
-    clearInterval(autoSlideInterval);
-    startAutoSlide();
-}
-
-//EVENTOS
-
-leftArrow.addEventListener('click', moveLeft);
-rightArrow.addEventListener('click', moveRight);
-window.addEventListener('resize', updateCarrossel);
-
-//CHAMA ÍNICO DAS FUNÇÕES
-updateCarrossel();
-startAutoSlide();
 
 // ---------- ANIMAÇÃO DO FIO ----------
 
@@ -105,7 +35,7 @@ gsap.utils.toArray(".item-fio").forEach((item) => {
     const itemTop = item.offsetTop;
     const containerHeight = document.querySelector(".container-fio").offsetHeight;
     
-    const startPercent = (itemTop / containerHeight) * 100;
+    const startPercent = ((itemTop + 50) / containerHeight) * 100;
 
     gsap.fromTo(
         item,
@@ -113,7 +43,7 @@ gsap.utils.toArray(".item-fio").forEach((item) => {
         {
             opacity: 1,
             y: 0,
-            duration: 0.5,
+            duration: 1,
             ease: "power2.out",
             scrollTrigger: {
                 trigger: ".container-fio",
