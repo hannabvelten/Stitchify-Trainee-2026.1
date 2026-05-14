410fc103c5ba2c4ef4c1b6ecf3112ec119ef44bd
hadassakelly <hadassak355@gmail.com>
2026-05-14 17:16:45 -0300

    feat: criação das funções de abrir e fachar modal, finalização do modal Criar Post

---
+++ public/js/modais.js
@@ -0,0 +1,13 @@
+const filtro = document.getElementById('filtro');
+
+function abriModal(idModal){
+    const modal = document.getElementById(idModal);
+    modal.style.display = "flex";
+    filtro.style.display = "flex";
+}
+
+function fecharModal(idModal){
+    const modal = document.getElementById(idModal);
+    modal.style.display = "none";
+    filtro.style.display = "none";
+}
