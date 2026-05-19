005b66c848daba52175c1e2aa35780cd4349f9e3
murilov1p <murilo.vilela@estudante.ufjf.br>
2026-05-19 18:34:01 -0300

    feat: adição de js, mudanças no css e html

---
+++ public/js/deletarusuario.js
@@ -0,0 +1,16 @@
+const openDeleteButton = document.querySelectorAll(".deletar");
+const closeDeleteButton = document.querySelector(".cancel-delete");
+const modalDelete = document.querySelector("#modal-deletar");
+const fadeDelete = document.querySelector("#fade-delete");
+
+const toggleModalDelete = () => {
+    [modalDelete, fadeDelete].forEach((el) => el.classList.toggle("hide"));
+};
+
+openDeleteButton.forEach((btn) => {
+    btn.addEventListener("click", () => toggleModalDelete());
+});
+
+[closeDeleteButton, fadeDelete].forEach((el) => {
+    el.addEventListener("click", () => toggleModalDelete());
+});
