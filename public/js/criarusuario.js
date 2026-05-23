ffc12949b432c3df2b5f2ef137a6e346ef60c901
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-05-23 19:05:08 -0300

    fiz o java script do botão de selecionar foto

--- public/js/criarusuario.js
+++ public/js/criarusuario.js
@@ -2,6 +2,21 @@ const abrirCriar = document.querySelector(".criar-usuario");
 const fecharCriar = document.querySelector(".x");
 const modalCriar = document.querySelector(".modal-criaruser");
 const fadeCriar = document.querySelector(".fade-criaruser");
+const botaoFoto = document.querySelector(".foto-usuario");
+const inputFoto = document.querySelector("#foto-usuario");
+
+botaoFoto.addEventListener("click", () => {
+    inputFoto.click();
+});
+
+inputFoto.addEventListener("change", () => {
+    const arquivo = inputFoto.files[0];
+    if (arquivo) {
+        console.log("Foto selecionada:", arquivo.name);
+    }
+
+}); 
+
 
 const toggleCriar = () => {
     modalCriar.classList.toggle("hide");
