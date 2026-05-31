1f228c4c7f52dd188faa8acc9e4aac69db8a3677
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-05-31 10:46:26 -0300

    feat: adiciona div foto-container

--- public/js/criarusuario.js
+++ public/js/criarusuario.js
@@ -4,7 +4,6 @@ const modalCriar = document.querySelector(".modal-criaruser");
 const fadeCriar = document.querySelector(".fade-criaruser");
 const botaoFoto = document.querySelector(".foto-usuario");
 const inputFoto = document.querySelector("#foto-usuario");
-const criarUsuariosBtn = document.querySelector("criar-modal");
 
 botaoFoto.addEventListener("click", () => {
     inputFoto.click();
@@ -18,6 +17,7 @@ inputFoto.addEventListener("change", () => {
 
 }); 
 
+
 const toggleCriar = () => {
     modalCriar.classList.toggle("hide");
 };
@@ -25,14 +25,3 @@ const toggleCriar = () => {
 [abrirCriar, fecharCriar, fadeCriar].forEach((el) => {
     el.addEventListener("click", toggleCriar);
 });
-
-criarUsuariosBtn.addEventListener("click", () =>{
-    const nome= document.querySelector(' .inputs-modal input[placeholder="Nome Completo"]').value;
-    const email= document.querySelector('.inputs-modal input[placeholder="Email"]').value;
-    const senha = document.querySelector(' .inputs-modal input[placeholder="Senha"]').value;
-    const foto = inputFoto.files[0];
-    if(!nome || !email || !senha){
-        console.log("Por favor, preencha todos os campos");
-        return;
-    }
-})
