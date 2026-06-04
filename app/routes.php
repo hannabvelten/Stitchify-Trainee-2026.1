3b51bde874db02533d791eb635a977d3494fc6ea
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-06-04 16:31:58 -0300

    feat: cria rota para o login

--- app/routes.php
+++ app/routes.php
@@ -4,4 +4,5 @@ namespace App\Controllers;
 use App\Controllers\ExampleController;
 use App\Core\Router;
 
-$router->get('', 'ExampleController@index');
+$router->get('', 'Controller@exibirLogin');
+$router->get('login', 'Controller@index');
