139d15f3436c92438e995885a5e2c7dcfd20d41c
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-06-04 12:51:31 -0300

    feat: procura e acha o modal respectivo do id

--- public/js/visualizar.js
+++ public/js/visualizar.js
@@ -1,5 +1,13 @@
 document.querySelectorAll('.btn-visualizar').forEach(botao => {
     botao.addEventListener('click', () => {
         const id = botao.getAttribute('data-id');
+        const modalEspecifico = document.getElementById(`modal-visualizar-${id}`);
+        const fadeEspecifico = modalEspecifico ? modalEspecifico.previousElementSibling : null;
+        if (modalEspecifico) {
+            modalEspecifico.classList.remove('hide');
+            if (fadeEspecifico && fadeEspecifico.classList.contains('fadev')) {
+                fadeEspecifico.classList.remove('hide');
+            }
+        }
     });
 });
