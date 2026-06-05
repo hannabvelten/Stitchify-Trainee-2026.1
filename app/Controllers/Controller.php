a1e99fc753a2f9a4c28d0320962ef6101493004d
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-06-05 10:59:35 -0300

    feat: adiciona metodo de exibir login e exibir landingpage

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -5,14 +5,19 @@ namespace App\Controllers;
 use App\Core\App;
 use Exception;
 
-class ExampleController
+class Controller
 {
 
     public function index()
     {
         return view('site/index');
     }
-    public function exibirLogin{
-        return view(name: 'admin/login');
+    public function exibirLogin()
+    {
+        return view('site/login');
+    }
+    public function exibirladingPage()
+    {
+        return view('site/landingpage');
     }
 }
