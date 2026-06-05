f887810b8a95e9c120eabf542e7a3a7d7b6bf96d
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-06-05 11:25:17 -0300

    feat: adiciona validação sucesso e erro no login

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -24,6 +24,18 @@ class Controller
     {
         $email = $_POST['email'];
         $senha = $_POST['senha'];
-        
+        $user = App::get('database')->verificaLogin($email, $senha);
+
+        if( $user != false){
+            session_start();
+            $_SESSION['id'] = $user->id;
+            header('Location: /dashboard');
+        }
+        else{
+            session_start();
+            $_SESSION['mensagem-erro'] = "Usuario e/ou senha incorretos";
+            header('Location: /login');
+        }
+
     }
 }
