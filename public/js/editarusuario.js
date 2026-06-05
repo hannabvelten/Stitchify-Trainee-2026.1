a36cc867c918cd332169455ab42c971bc876d611
murilov1p <murilo.vilela@estudante.ufjf.br>
2026-06-05 14:59:16 -0300

    feat: edit foto js

--- public/js/editarusuario.js
+++ public/js/editarusuario.js
@@ -2,6 +2,10 @@ const openEditButton = document.querySelectorAll(".editar");
 const closeEditButton = document.querySelector(".modal-editar .cancel");
 const modalEdit = document.querySelector("#modal-editar");
 const fadeEdit = document.querySelector("#fade-edit");
+const btnFotoEdit = document.querySelector("#btnFotoEdit");
+const inputFotoEdit = document.querySelector("#inputFotoEdit");
+const fotoIconEdit = document.querySelector("#fotoIconEdit");
+const fotoPreviewEdit = document.querySelector("#fotoPreviewEdit");
 
 const toggleModalEdit = () => {
     [modalEdit, fadeEdit].forEach((el) => el.classList.toggle("hide"));
@@ -13,4 +17,18 @@ openEditButton.forEach((btn) => {
 
 [closeEditButton, fadeEdit].forEach((el) => {
     el.addEventListener("click", () => toggleModalEdit());
+});
+
+btnFotoEdit.addEventListener("click", () => {
+    inputFotoEdit.click();
+});
+
+inputFotoEdit.addEventListener("change", () => {
+    const arquivo = inputFotoEdit.files[0];
+    if (arquivo) {
+        const url = URL.createObjectURL(arquivo);
+        fotoIconEdit.style.display = "none";
+        fotoPreviewEdit.style.display = "block";
+        fotoPreviewEdit.src = url;
+    }
 });
