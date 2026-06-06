f836b56c9dd2f2ba495a9074a008aec66d035661
hadassakelly <hadassak355@gmail.com>
2026-06-06 13:34:34 -0300

    fix: tabela usarios no querybuilder

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -38,7 +38,7 @@ class QueryBuilder
         $sql = "SELECT 
                 tabela_posts.*,
                 tabela_usuarios.nome AS nome_autor,
-                tabela_usuarios.imagem AS foto_autor
+                tabela_usuarios.foto AS foto_autor
                 FROM tabela_posts
                 INNER JOIN tabela_usuarios ON tabela_posts.autor = tabela_usuarios.id_usuario";
 
@@ -91,7 +91,7 @@ class QueryBuilder
         $sql = "SELECT 
                 tabela_posts.*,
                 tabela_usuarios.nome AS nome_autor,
-                tabela_usuarios.imagem AS foto_autor
+                tabela_usuarios.foto AS foto_autor
             FROM tabela_posts
             INNER JOIN tabela_usuarios ON tabela_posts.autor = tabela_usuarios.id_usuario
             LIMIT {$limit} OFFSET {$offset}";
