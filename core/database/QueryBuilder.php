a19de6c76486dd131da1e15af2fc1ea57e4a365b
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-06-14 11:34:42 -0300

    fix: adiciona cifrão em params

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -9,7 +9,7 @@ class QueryBuilder
     protected $pdo;
     protected $table;
     protected $conditions = [];
-    protected params = [];
+    protected $params = [];
 
     public function __construct($pdo)
     {
