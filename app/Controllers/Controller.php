9eb254a914799b0d81fd5b3c74a84c6922b70a7a
MariaLuiza <mariadias.luiza@estudante.ufjf.br>
2026-06-19 09:21:22 -0300

    fix: ajusta nome do arquivo para abertura da lading page no controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -22,7 +22,7 @@ class Controller
     }
     public function exibirlandingPage()
     {
-        return view('site/landingpage');
+        return view('site/landing-page');
     }
     public function efetuaLogin()
     {
