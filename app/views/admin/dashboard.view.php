9524331df637b9754772bb613032195c55282c1a
hadassakelly <hadassak355@gmail.com>
2026-06-21 17:46:20 -0300

    feat: botao home na dashboard

--- app/views/admin/dashboard.view.php
+++ app/views/admin/dashboard.view.php
@@ -20,20 +20,33 @@
                     <h3>Total de publicações: <?= $totalPublicacoes ?></h3>
                 </div>
             </div>
-
-            <div class="admin-profile">
-                <div class="profile-icon">
-                    <?php if (!empty($usuario->foto)): ?>
-                  <img src="../../../public/uploads/<?= htmlspecialchars($usuario->foto) ?>" alt="Foto de <?= htmlspecialchars($usuario->nome) ?>"         
-                 />
-                  <?php else: ?>
-                  <img src="../../../public/assets/PersonCircle.svg" alt="Sem foto"  />
-                  <?php endif; ?>                </div>
-                <div class="caixa-admin">
-                        <h4><?= $admin->nome?></h4>
-                        <p>Administrador</p>
+            <div class="admin-card">
+                <div class="admin-profile">
+                    
+                    <div class="profile-icon">
+                        <?php if (!empty($usuario->foto)): ?>
+                    <img src="../../../public/uploads/<?= htmlspecialchars($usuario->foto) ?>" alt="Foto de <?= htmlspecialchars($usuario->nome) ?>"         
+                    />
+                    <?php else: ?>
+                    <img src="../../../public/assets/PersonCircle.svg" alt="Sem foto"  />
+                    <?php endif; ?>                </div>
+                    <div class="caixa-admin">
+                            <h4><?= $admin->nome?></h4>
+                            <p>Administrador</p>
+                    </div>
+                </div>
+                <div class="admin-home">
+                    <a href="/">
+                        <svg class="home" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-icon lucide-house">
+                            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
+                            <path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+                        </svg>
+                        <p>Home</p>
+                    </a>
+                        
                 </div>
             </div>
+                
         </header>
 
         <div class="titulo">
