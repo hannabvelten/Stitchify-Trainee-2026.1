7d0f1b88fe6be28ab311488c7a257f10ba8767c4
GustavoBgs21 <gustavosborges21@gmail.com>
2026-06-21 06:18:56 -0300

    feat: ajusta navbar do usuario logado

--- app/views/site/navbar.php
+++ app/views/site/navbar.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stitchify</title>
-    <link rel="stylesheet" href="../../../public/css/navbar.css">
-</head>
-<body>
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<link rel="stylesheet" href="../../../public/css/navbar.css">
+
+<?php if (isset($_SESSION['id'])): ?>
+    <link rel="stylesheet" href="../../../public/css/navbarusuario.css">
+
     <header class="navbar">
         <div class="navbar-logo">
             <img src="../../../public/assets/logo_1.png" alt="Logo Stitchify">
         </div>
+
         <nav class="navbar-links">
             <a href="/landing-page">
                 <span class="link-icon">
@@ -20,6 +23,7 @@
                 </span>
                 <span>Home</span>
             </a>
+
             <a href="/lista-de-posts">
                 <span class="link-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-card-heading" viewBox="0 0 16 16">
@@ -30,24 +34,65 @@
                 <span>Publicações</span>
             </a>
 
-            <?php if(isset($_SESSION['id_usuario'])):?>
-                <a href="/pagusuario">
-                    <img src="../../../public/assets/usuarios/<?= htmlspecialchars($_SESSION['foto']) ?>" alt="">
-                    <span><?= htmlspecialchars($_SESSION['nome']) ?></span>
-                </a>
-                <a href="/logout">Sair</a>
-            <?php else: ?> 
-                <a href="/login">
+            <a href="/perfil-usuario">
+                <span class="link-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.831 1.418-.832 1.664z"/>
+                    </svg>
+                </span>
+                <span>Meu Perfil</span>
+            </a>
+
+            <form action="/logout" method="POST" class="form-sair">
+                <button type="submit" class="botao-sair">
                     <span class="link-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 0a4 4 0 0 1 4 4v2.05a2.5 2.5 0 0 1 2 2.45v5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5v-5a2.5 2.5 0 0 1 2-2.45V4a4 4 0 0 1 4-4M4.5 7A1.5 1.5 0 0 0 3 8.5v5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 11.5 7zM8 1a3 3 0 0 0-3 3v2h6V4a3 3 0 0 0-3-3"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
                         </svg>
                     </span>
-                    <span>Login</span>
-                </a>
-            <?php endif; ?>
+                    <span>Sair</span>
+                </button>
+            </form>
         </nav>
     </header>
-    
-</body>
-</html>
+
+<?php else: ?>
+
+    <header class="navbar">
+        <div class="navbar-logo">
+            <img src="../../../public/assets/logo_1.png" alt="Logo Stitchify">
+        </div>
+
+        <nav class="navbar-links">
+            <a href="/landing-page">
+                <span class="link-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+                    </svg>
+                </span>
+                <span>Home</span>
+            </a>
+
+            <a href="/lista-de-posts">
+                <span class="link-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-card-heading" viewBox="0 0 16 16">
+                        <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                        <path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                </span>
+                <span>Publicações</span>
+            </a>
+
+            <a href="/login">
+                <span class="link-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 0a4 4 0 0 1 4 4v2.05a2.5 2.5 0 0 1 2 2.45v5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5v-5a2.5 2.5 0 0 1 2-2.45V4a4 4 0 0 1 4-4M4.5 7A1.5 1.5 0 0 0 3 8.5v5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 11.5 7zM8 1a3 3 0 0 0-3 3v2h6V4a3 3 0 0 0-3-3"/>
+                    </svg>
+                </span>
+                <span>Login</span>
+            </a>
+        </nav>
+    </header>
+
+<?php endif; ?>
