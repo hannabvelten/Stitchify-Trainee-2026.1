63212b7cf14b9a85afd562eca4b390412efeed20
hadassakelly <hadassak355@gmail.com>
2026-06-21 19:24:15 -0300

    fix: metodo get no logout

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@ $router->get('login', 'Controller@exibirLogin');
 $router->get('inscreva-se', 'Controller@exibirInscrevaSe');
 $router->get('landing-page', 'Controller@exibirlandingPage');
 $router->post('login', 'Controller@efetuaLogin');
-$router->post('logout', 'Controller@efetuaLogout');
+$router->get('logout', 'Controller@efetuaLogout');
 $router->post('inscreva-se', 'Controller@efetuaInscricao');
 
 
