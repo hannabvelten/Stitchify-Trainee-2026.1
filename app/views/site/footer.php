ca21bb36320fa23316742abca546912f42f0421b
hadassakelly <hadassak355@gmail.com>
2026-06-21 19:19:26 -0300

    feat: adiciona logo code no footer

--- app/views/site/footer.php
+++ app/views/site/footer.php
@@ -18,11 +18,16 @@
                     <a href="#"><img src="/public/assets/logoinstagram.png" alt="logo-instagram"></a>
                     <a href="#"><img src="/public/assets/logoyoutube.png" alt="logo-youtube"></a>
                     <a href="#"><img src="/public/assets/logofacebook.png" alt="logo-facebook"></a>
+                    <a href="https://www.codejr.com.br/"><img src="../../../public/assets/icon-code2.svg" alt=""></a>
                 </div>
             </div>
 
             <div class="linha1"></div>
-            <h3 id="sobre-nos"><a href="/landing-page#sobre">Sobre nós</a></h3>
+            
+            <div class="logo-sobre">
+                <h3 id="sobre-nos"><a href="/landing-page#sobre">Sobre nós</a></h3>
+            </div>
+            
             <div class="linha2"></div>
             <div class="imagem-logo">
                 <img src="../../../public/assets/logo-stitchify.png" alt="Logo Stitchify">
